bdd88db8164ed3bf7e47cf1c827c8422fdf23cb0
Lenny-Prosper <leonardmalcolm00@gmail.com>
2023-09-28 04:24:50 +0100

    Initialized app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="w-screen h-[100dvh] bg-[#F9FAFF] flex leading-normal overflow-x-hidden">
+    </div>
+  )
+}
+
+export default App
